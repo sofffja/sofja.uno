24df8ad4052d8d40722fac92e71bd695b43027c5
sofffja <sofi.efron@gmail.com>
2024-07-23 13:49:54 -0300

    Convert images to .webp

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,36 +2,36 @@ import Obra from './Obra';
 
 export default function Gallery() {
   const obras = [
-    { title: 'mas que anibios', img: 'n006.jpg', text: '' },
+    { title: 'mas que anibios', img: 'n006.webp', text: '' },
     {
       title: 'cover artwork for Stazma The Junglechrist - Poursuite EP',
-      img: 'T010b.jpg',
+      img: 'T010b.webp',
       text: '',
     },
     {
       title: 'cover artwork for Sufrimiento Records V/A',
-      img: 'w003.jpg',
+      img: 'w003.webp',
       text: '',
     },
     {
       title: 'cover artwork for Cimarron - Sin Cara, Sin Nombre',
-      img: 'cimarron-1920.jpg',
+      img: 'cimarron-1920.webp',
       text: '',
     },
     {
       title: '[▒▒▒╜░╜╙▒▒▒▒▒▒▒]',
-      img: 'n011.jpg',
+      img: 'n011.webp',
       text: `SA001 ☽•∷○
       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓`,
     },
     {
       title: 'in a pool of their own juices',
-      img: 'n010.jpg',
+      img: 'n010.webp',
       text: 'sin ojos ni oídos, sin piernas',
     },
     {
       title: 'anew',
-      img: 'n009.jpg',
+      img: 'n009.webp',
       text: `the whole body is shaking, but there is a quiet feeling inside
       a flame burning at the feet of his feet
       its flames rising by its fires and its fire rising by its flames
@@ -41,7 +41,7 @@ export default function Gallery() {
     },
     {
       title: 'como bichos arrojados en la noche',
-      img: 'n007.jpg',
+      img: 'n007.webp',
       text: `sharp horns that can hide behind some thick cloth
       bodies so dense it's barely visible
       it disappeared from the sea and returned to Earth
@@ -49,10 +49,10 @@ export default function Gallery() {
       new eyes
       could have just seen it for the first time`,
     },
-    { title: 'landscape type II', img: 'n008.jpg', text: '' },
-    { title: '', img: 'c001.jpg', text: '' },
-    { title: '', img: 'c002.jpg', text: '' },
-    { title: '', img: 'c003.jpg', text: '' },
+    { title: 'landscape type II', img: 'n008.webp', text: '' },
+    { title: '', img: 'c001.webp', text: '' },
+    { title: '', img: 'c002.webp', text: '' },
+    { title: '', img: 'c003.webp', text: '' },
   ];
 
   return (
