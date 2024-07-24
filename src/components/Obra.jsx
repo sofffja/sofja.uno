2cac02158b0846007ebdb35e62c67e5eb1ab4b2e
sofffja <sofi.efron@gmail.com>
2024-07-24 19:05:47 -0300

    Add animation to artwork when in view

--- src/components/Obra.jsx
+++ src/components/Obra.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 
 export default function Obra({ obra }) {
   const [isLoaded, setIsLoaded] = useState(false);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
   if (!obra) return null;
 
@@ -10,10 +13,13 @@ export default function Obra({ obra }) {
       key={obra.title}
       className="items-center justify-center w-full min-h-[calc(100vh+3rem)] flex flex-col gap-2"
     >
-      <img
+      <motion.img
         src={`media/${obra.img}`}
         alt={obra.title}
         onLoad={() => setIsLoaded(true)}
+        ref={ref}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isInView ? 1 : 0 }}
         className={`rounded-md shadow-[0_0_18px_0_rgba(0,0,0,0.3)] max-h-[80vh] h-auto max-w-2/3 lg:max-w-2/5 ${
           isLoaded ? 'opacity-100' : 'opacity-0'
         } transition-opacity`}
