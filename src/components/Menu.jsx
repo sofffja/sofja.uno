b92676ab7e802651f12239fdab9006da6ff8e10e
sofffja <sofi.efron@gmail.com>
2024-08-10 13:26:19 -0300

    Refactor

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,26 +5,27 @@ export default function Menu() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 flex gap-2 md:gap-4 drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)] uppercase text-xl md:text-2xl pr-[2px] leading-none w-full mix-blend-difference ">
-        <h1 className="mr-auto">Sofja</h1>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? baseStyle + ' line-through' : baseStyle
-          }
-        >
-          index
-        </NavLink>
-
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive ? baseStyle + ' line-through' : baseStyle
-          }
-        >
-          about
-        </NavLink>
-      </nav>
+      <header className="fixed top-0 left-0 flex justify-between drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)] uppercase text-xl md:text-2xl px-1 leading-none w-full mix-blend-difference ">
+        <h1 className="">Sofja</h1>
+        <nav className="flex gap-2 md:gap-4">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? baseStyle + ' line-through' : baseStyle
+            }
+          >
+            index
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? baseStyle + ' line-through' : baseStyle
+            }
+          >
+            about
+          </NavLink>
+        </nav>
+      </header>
     </>
   );
 }
