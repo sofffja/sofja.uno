8e93c476baea2ea24d137c997a12350a5f341341
sofffja <sofi.efron@gmail.com>
2024-09-09 23:13:13 -0300

    Update about and menu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { NavLink } from 'react-router-dom';
 
 export default function About() {
   const baseStyle = 'hover:text-primary transition-colors';
@@ -8,32 +9,32 @@ export default function About() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="mt-6 md:mt-8 drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)] flex md:flex-row flex-col justify-between lg:w-3/4 xl:w-2/3"
+      className="mt-6 md:mt-8 drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)]"
     >
-      <p>digital artist && web developer && electronic musician</p>
-
-      <div className="flex gap-4">
-        <p>contact: </p>
-        <div className="flex flex-col">
-          <a className={baseStyle} href="mailto:sfffj@proton.me">
-            sfffj@proton.me
-          </a>
-          <a className={baseStyle} href="https://www.instagram.com/_sofffja/">
-            instagram
-          </a>
-          <a className={baseStyle} href="https://twitter.com/_sofffja">
-            twitter
-          </a>
-          <a className={baseStyle} href="https://soundcloud.com/sofffja">
-            soundcloud
+      <div className="flex md:grid grid-cols-3 place-items-center flex-col justify-between">
+        <p>visuals && music && code</p>
+        <div className="flex gap-4">
+          <p>contact: </p>
+          <div className="flex flex-col">
+            <a className={baseStyle} href="mailto:sfffj@proton.me">
+              sfffj@proton.me
+            </a>
+            <a className={baseStyle} href="https://www.instagram.com/_sofffja/">
+              instagram
+            </a>
+            <a className={baseStyle} href="https://twitter.com/_sofffja">
+              twitter
+            </a>
+            <a className={baseStyle} href="https://soundcloud.com/sofffja">
+              soundcloud
+            </a>
+          </div>
+        </div>
+        <div>
+          <a className={baseStyle} href="https://www.aguja.re/">
+            +++ aguja.re
           </a>
         </div>
-      </div>
-
-      <div>
-        <a className={baseStyle} href="https://www.aguja.re/">
-          +++ aguja.re
-        </a>
       </div>
     </motion.div>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,9 @@ export default function Menu() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 flex justify-between drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)] uppercase text-xl md:text-2xl px-1 leading-none w-full mix-blend-difference ">
-        <h1 className="">Sofja</h1>
-        <nav className="flex gap-2 md:gap-4">
+      <header className="fixed top-0 left-0 flex justify-between drop-shadow-[1px_1px_0px_rgba(0,0,0,0.5)] uppercase px-1 leading-none w-full mix-blend-difference ">
+        <h1 className="text-xl md:text-2xl">Sofja</h1>
+        <nav className="flex gap-2 md:gap-4 pt-1">
           <NavLink
             to="/"
             className={({ isActive }) =>
