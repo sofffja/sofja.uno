1ef1d28e91528b4c9fb8c329263c171437404777
sofffja <sofi.efron@gmail.com>
2024-10-05 13:24:38 -0300

    Update image animations on load

--- src/components/Obra.jsx
+++ src/components/Obra.jsx
@@ -14,15 +14,17 @@ export default function Obra({ obra }) {
       className="items-center justify-center w-full sm:min-h-[calc(100vh+3rem)] text-[#777] flex flex-col gap-2"
     >
       <motion.img
+        ref={ref}
         src={`media/${obra.img}`}
         alt={obra.title}
-        onLoad={() => setIsLoaded(true)}
-        ref={ref}
         initial={{ opacity: 0 }}
-        animate={{ opacity: isInView ? 1 : 0 }}
-        className={`rounded-md shadow-[1px_1px_0_0_#00000077,-1px_-1px_0_0_#ffffff77] max-h-[80vh] h-auto max-w-2/3 lg:max-w-2/5 ${
-          isLoaded ? 'opacity-100' : 'opacity-0'
-        } transition-opacity`}
+        animate={{
+          opacity: isInView && isLoaded ? 1 : 0,
+          transition: { duration: 0.6, ease: 'easeInOut', delay: 0.3 },
+        }}
+        className="rounded-md shadow-[1px_1px_0_0_#00000077,-1px_-1px_0_0_#ffffff77] max-h-[80vh] h-auto max-w-2/3 lg:max-w-2/5"
+        onLoad={() => setInterval(() => setIsLoaded(true), 1000)}
+        loading="lazy"
       />
       <p className="drop-shadow-[1px_1px_0px_#fff] ">{obra.title}</p>
       <p className="drop-shadow-[1px_1px_0px_#fff] whitespace-pre-line text-sm">
